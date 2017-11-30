a97551f2037215c3528589e9bb83f09f0df3b116
alicemw <1660523599@qq.com>
2017-11-30 17:40:36 +0800

    登录界面

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -2,20 +2,24 @@
 	<div class="login">
 		<el-card class="box-card">
 		  <div slot="header" class="clearfix">
-		    <span>系统登录</span>
-		    <el-button style="float: right; padding: 3px 0" type="text">注册</el-button>
+		    <span>系统{{title}}</span>
+		    <el-button @click="changeLogin()" style="float: right; padding: 3px 0" type="text"  ><i class="el-icon-refresh"></i>{{title=="登录"? "注册" : "登录"}}</el-button>
 		  </div>
-		  <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="40px" class="demo-ruleForm">
-			  <el-form-item label="账号" prop="pass">
-			    <el-input type="text" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+		  <el-form  :model="loginform" :rules="rule1" status-icon ref="loginform"  :label-position="labelPosition" :label-width="labelWidth" class="demo-ruleForm">
+			  <el-form-item label="账号" prop="user">
+			    <el-input type="text" v-model="loginform.user" placeholder="请输入账号" ></el-input>
 			  </el-form-item>
-			  <el-form-item label="密码" prop="checkPass">
-			    <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+			  <el-form-item label="密码" prop="pass1">
+			    <el-input type="password" v-model="loginform.pass1" placeholder="请输入密码" ></el-input>
+			  </el-form-item>
+			  
+			  <el-form-item label="确认密码" v-show="confirmPass" prop="pass2">
+			    <el-input type="password" v-model="loginform.pass2" placeholder="请确认密码" ></el-input>
 			  </el-form-item>
 			  
 			  <el-form-item>
-			    <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
-			    <el-button @click="resetForm('ruleForm2')">重置</el-button>
+			    <el-button type="primary" @click="submit()">提交</el-button>
+			    <el-button type="danger" @click="reset('loginform')">重置</el-button>
 			  </el-form-item>
 			</el-form>
 		</el-card>
@@ -25,75 +29,77 @@
 
 <script>
 export default {
-    data() {
-      var checkAge = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('年龄不能为空'));
-        }
-        setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'));
-          } else {
-            if (value < 18) {
-              callback(new Error('必须年满18岁'));
-            } else {
-              callback();
-            }
-          }
-        }, 1000);
-      };
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入账号'));
-        } else {
-          if (this.ruleForm2.checkPass !== '') {
-            this.$refs.ruleForm2.validateField('checkPass');
-          }
-          callback();
-        }
-      };
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'));
-        }else {
-          callback();
-        }
-      };
-      return {
-        ruleForm2: {
-          pass: '',
-          checkPass: '',
-          age: ''
-        },
-        rules2: {
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          age: [
-            { validator: checkAge, trigger: 'blur' }
-          ]
-        }
-      };
-    },
-    methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
-    }
-  }
+	data(){
+		var checkUser = (rule,value,callback)=>{
+			if(!value){
+				return callback(new Error("账号不能为空！"));
+			}else if(value.length>5||value.length<3){
+				return callback(new Error("账号必须在6-10个字符之间！"));
+			}else {
+				callback()
+			}
+		};
+		var checkPass = (rule,value,callback)=>{
+			if (value === '') {
+	          callback(new Error('请输入密码'));
+	        } else {
+	          if (this.loginform.pass1 !== '') {
+	            this.$refs.loginform.validateField('pass');
+	          }
+	          callback();
+	        }
+		};
+		 var checkPass2 = (rule, value, callback) => {
+		 	
+	        if (value === '') {
+	          callback(new Error('请再次输入密码'));
+	        } else if (value !== this.loginform.pass1) {
+	          callback(new Error('两次输入密码不一致!'));
+	        } else {
+	          callback();
+	        }
+	      };
+		
+		
+		return {
+			labelPosition:'right',
+			confirmPass:false,
+			labelWidth:'40px',
+			title:'登录',
+			
+			loginform:{
+				user:'',
+				pass1:'',
+				pass2:''
+			},
+			rule1:{
+				user:[
+					{validator: checkUser, trigger: 'blur'}
+				],
+				pass1:[
+					{validator: checkPass, trigger: 'blur'}
+				],
+				pass2:[
+					{validator: checkPass2, trigger: 'blur'}
+				]
+				
+			}
+		}
+	},
+	methods:{
+		changeLogin () {
+			this.title =='登录'? this.title = '注册' : this.title = '登录';
+			this.confirmPass = !this.confirmPass;
+			this.labelWidth == '40px' ? this.labelWidth = '70px': this.labelWidth = '40px';
+		},
+		submit(){
+			 
+		},
+		reset(formName){//ref 绑定表单对象， prop绑定需要清除的表单元素item
+			this.$refs[formName].resetFields();
+		}
+	}
+}
 </script>
 
 <style lang="less">
